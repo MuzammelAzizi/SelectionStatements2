4cf144e1ae7f87e30b86625362ea2ff5c8a3fb8c
Muzammel Osmani Azizi <ms7.swaggy@gmail.com>
2024-02-20 11:41:29 -0800

    status SelectionStatements2

---
+++ SelectionStatements2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Mail;
+
+namespace SelectionStatements2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your favorite subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Science":
+                    Console.WriteLine("Science is an interesting subject");
+                    break;
+
+                case "Art":
+                    Console.WriteLine("Art is a fun!");
+                    break;
+
+                case "Math":
+                    Console.WriteLine("Math is a hard subject");
+                    break;
+
+                case "English":
+                    Console.WriteLine("English is an amazing subject!"); 
+                    break;
+
+                case "Chemistry":
+                    Console.WriteLine("Chemistry is a tough subject");  
+                    break;
+
+                default : Console.WriteLine("Amazing!! I remember taking that subject");    
+                    break;
+
+
+            }
+        }
+    }
+}
